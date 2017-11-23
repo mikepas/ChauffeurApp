062b69af0bfd654b24425646838b729c9ded5067
Pas, Mike <PS164043@summacollege.nl>
2017-11-23 23:25:41 +0100

    Changed icon

--- ChauffeurApp/ChauffeurApp.Android/MainActivity.cs
+++ ChauffeurApp/ChauffeurApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ChauffeurApp.Droid
 {
-    [Activity(Label = "Chauffeurs App", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Chauffeurs App", Icon = "@drawable/logochauffeursapp", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
